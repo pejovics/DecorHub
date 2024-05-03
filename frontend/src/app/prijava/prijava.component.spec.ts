b481bf5b45ca25fd588d78f2a89b1c945e3f2ab9
stefanpejovic <stefan.pejovic@zuehlke.com>
2024-05-03 18:14:49 +0200

    Vracanje zakomentarisanih testova

--- frontend/src/app/prijava/prijava.component.spec.ts
+++ frontend/src/app/prijava/prijava.component.spec.ts
@@ -1,23 +1,23 @@
-// import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-// import { PrijavaComponent } from './prijava.component';
-// import { FormsModule } from '@angular/forms';
+import { PrijavaComponent } from './prijava.component';
+import { FormsModule } from '@angular/forms';
 
-// describe('PrijavaComponent', () => {
-//   let component: PrijavaComponent;
-//   let fixture: ComponentFixture<PrijavaComponent>;
+describe('PrijavaComponent', () => {
+  let component: PrijavaComponent;
+  let fixture: ComponentFixture<PrijavaComponent>;
 
-//   beforeEach(() => {
-//     TestBed.configureTestingModule({
-//       declarations: [PrijavaComponent],
-//       imports: [FormsModule]
-//     });
-//     fixture = TestBed.createComponent(PrijavaComponent);
-//     component = fixture.componentInstance;
-//     fixture.detectChanges();
-//   });
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [PrijavaComponent],
+      imports: [FormsModule]
+    });
+    fixture = TestBed.createComponent(PrijavaComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
 
-//   it('should create', () => {
-//     expect(component).toBeTruthy();
-//   });
-// });
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
